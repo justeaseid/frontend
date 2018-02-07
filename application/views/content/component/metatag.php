ff1aeba80e1924ae6176c963c7ba11983f6d587a
paramafadli <parama.fadli@gmail.com>
2018-02-07 09:14:39 +0700

    FE-02 update metatag

--- application/views/content/component/metatag.php
+++ application/views/content/component/metatag.php
@@ -1,6 +1,6 @@
 <meta charset="utf-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
-<title>justease - Donasi online untuk kasus hukum</title>
+<title>justease - Donasi untuk Kasus Hukum</title>
 <link rel="shortcut icon" href="<?php echo ICON; ?>" />
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <meta name="mobile-web-app-capable" content="yes">
@@ -8,12 +8,12 @@
 <meta name="apple-mobile-web-app-capable" content="yes">
 <meta name="apple-mobile-web-app-title" content="justease">
 <meta name="description" content="justease Platform galang dana dan donasi online untuk kasus hukum untuk siapapun & di manapun">
-<meta name="twitter:image" content="pic/logos.jpg">
+<meta name="twitter:image" content="<?php echo ICON; ?>">
 <meta property="og:site_name" content="justease">
 <meta property="og:locale" content="id_ID">
 <meta property="og:image:width" content="100">
 <meta property="og:image:height" content="100">
-<meta property="og:image" content="pic/logos.jpg">
+<meta property="og:image" content="<?php echo ICON; ?>">
 <meta property="og:title" content="#legalhero bersama justease!">
 <meta property="og:description" content="justease Platform galang dana dan donasi online untuk kasus hukum untuk siapapun & di manapun">
 <meta property="og:card" content="justease Platform galang dana dan donasi online untuk kasus hukum untuk siapapun & di manapun">
