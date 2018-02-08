ea8714ad98610e916aac90148815ee182f346567
paramafadli <parama.fadli@gmail.com>
2018-02-08 17:41:58 +0700

    FE-23 add css on footer social media

--- application/views/layout/footer/footer_social.php
+++ application/views/layout/footer/footer_social.php
@@ -7,11 +7,11 @@
             <div class="col-sm-12 text-center">
                 <ul class="list-inline">
                     <li>
-                        <a href="#" class="btn btn-social btn-outline"><span class="sr-only">Facebook</span><i class="fa fa-facebook"></i></a>
-                        <a href="#" class="btn btn-social btn-outline"><span class="sr-only">Google Plus</span><i class="fa fa-google-plus"></i></a>
-                        <a href="#" class="btn btn-social btn-outline"><span class="sr-only">Twitter</span><i class="fa fa-twitter"></i></a>
-                        <a href="#" class="btn btn-social btn-outline"><span class="sr-only">Linked In</span><i class="fa fa-linkedin"></i></a>
-                        <a href="#" class="btn btn-social btn-outline"><span class="sr-only">Youtube</span><i class="fa fa-youtube"></i></a>
+                        <a href="#" class="btn btn-social btn-outline" style="border: none;"><span class="sr-only">Facebook</span><i class="fa fa-facebook"></i></a>
+                        <a href="#" class="btn btn-social btn-outline" style="border: none;"><span class="sr-only">Google Plus</span><i class="fa fa-google-plus"></i></a>
+                        <a href="#" class="btn btn-social btn-outline" style="border: none;"><span class="sr-only">Twitter</span><i class="fa fa-twitter"></i></a>
+                        <a href="#" class="btn btn-social btn-outline" style="border: none;"><span class="sr-only">Linked In</span><i class="fa fa-linkedin"></i></a>
+                        <a href="#" class="btn btn-social btn-outline" style="border: none;"><span class="sr-only">Youtube</span><i class="fa fa-youtube"></i></a>
                     </li>
                 </ul>
             </div>
