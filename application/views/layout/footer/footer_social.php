4226a80b3c621be34302080803c1c41d8349ddcb
paramafadli <parama.fadli@gmail.com>
2018-02-10 19:13:59 +0700

    FE-30 add link on footer social media

--- application/views/layout/footer/footer_social.php
+++ application/views/layout/footer/footer_social.php
@@ -7,11 +7,30 @@
             <div class="col-sm-12 text-center">
                 <ul class="list-inline">
                     <li>
-                        <a href="#" class="btn btn-social btn-outline" style="border: none;"><span class="sr-only">Facebook</span><i class="fa fa-facebook"></i></a>
-                        <a href="#" class="btn btn-social btn-outline" style="border: none;"><span class="sr-only">Google Plus</span><i class="fa fa-google-plus"></i></a>
-                        <a href="#" class="btn btn-social btn-outline" style="border: none;"><span class="sr-only">Twitter</span><i class="fa fa-twitter"></i></a>
-                        <a href="#" class="btn btn-social btn-outline" style="border: none;"><span class="sr-only">Linked In</span><i class="fa fa-linkedin"></i></a>
-                        <a href="#" class="btn btn-social btn-outline" style="border: none;"><span class="sr-only">Youtube</span><i class="fa fa-youtube"></i></a>
+                        <a href="https://www.facebook.com/Justeaseid-2072810949632298/" target="_blank" class="btn btn-social btn-outline" style="border: none;">
+                            <span class="sr-only">Facebook</span>
+                            <i class="fa fa-facebook"></i>
+                        </a>
+                        <a href="https://plus.google.com/113913426928656533324" target="_blank" class="btn btn-social btn-outline" style="border: none;">
+                            <span class="sr-only">Google Plus</span>
+                            <i class="fa fa-google-plus"></i>
+                        </a>
+                        <a href="https://twitter.com/justeaseid" target="_blank" class="btn btn-social btn-outline" style="border: none;">
+                            <span class="sr-only">Twitter</span>
+                            <i class="fa fa-twitter"></i>
+                        </a>
+                        <a href="https://www.linkedin.com/in/justeaseid/" target="_blank" class="btn btn-social btn-outline" style="border: none;">
+                            <span class="sr-only">Linked In</span>
+                            <i class="fa fa-linkedin"></i>
+                        </a>
+                        <a href="https://www.youtube.com/channel/UCHAdOj1X-cDyWxnYxwdDDgQ" target="_blank" class="btn btn-social btn-outline" style="border: none;">
+                            <span class="sr-only">Youtube</span>
+                            <i class="fa fa-youtube"></i>
+                        </a>
+                        <a href="https://www.instagram.com/justeaseid/" target="_blank" class="btn btn-social btn-outline" style="border: none;">
+                            <span class="sr-only">Instagram</span>
+                            <i class="fa fa-instagram"></i>
+                        </a>
                     </li>
                 </ul>
             </div>
